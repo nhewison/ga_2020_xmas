a5cfa7588a577d48b025732ca92298ad505e72ee
Natalie Hewison <nataliehewison@hotmail.co.uk>
2020-11-30 21:28:24 +0000

    isPolaroid Function

--- index.js
+++ index.js
@@ -24,23 +24,23 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // An array of the doors
   const doorsObject = [
-    { number: '1', img: './Images/MB.png', text: ' "Calm is a superpower"', name: '- Michelle' },
     { number: '2', img: './Images/SP.png', text: '"Ahoy there everyone! A highlight for me was FL Appreciation Week and obviously being Ned for a day!"', name: '- Somerset' },
-    { number: '3', img: './Images/DC.png', text: '"Does work always have to be serious? Neigh. These two amazing people surprised me, yelled "haaaaaaay" and it made my day. As they say, you can take a horse to water, but you cant make it call you on Zoom."', name: '- Callum ' },
-    { number: '4', img: './Images/CAL.png', text: '"The debut of the Caltato"', name: '- Kremmy' },
     { number: '5', img: './Images/JF.png', text: '“Im a survivor, Im not gonna give up, Im not gonna stop, Im gonna work harder, Im a survivor, Im gonna make it, I will survive, keep on surviving”', name: '- John'},
+    { number: '1', img: './Images/MB.png', text: ' "Calm is a superpower"', name: '- Michelle' },
     { number: '6', img: './Images/YA.png', text: '"Business as usual!"', name:'- Antonio' },
     { number: '7', img: './Images/EM.png', text: '"Dont forget to believe in a little magic!"', name:'- Ellen' },
+    { number: '17', img: './Images/CS.png', text: 'text', name:'name' },
     { number: '8', img: './Images/FW.png', text: '"Vintage is just overpriced retro" - Somerset Prew (in reference to how to refer to Brexit) ', name:'- Fi' },
     { number: '9', img: './Images/ELF.png', text: '"Be ready to face any challenge that may be foolish enough to face you." ', name:'- Dwight' },
     { number: '10', img: './Images/NH.png', text: '"The secret of change is to focus all of your energy, not on fighting the old, but on building the new." - Socrates', name:'- Nat' },
+    { number: '3', img: './Images/DC.png', text: '"Does work always have to be serious? Neigh. These two amazing people surprised me, yelled "haaaaaaay" and it made my day. As they say, you can take a horse to water, but you cant make it call you on Zoom."', name: '- Callum ' },
     { number: '11', img: './Images/AI.png', text: '"Shout out to Antonios MO!"', name:'- Chip' },
     { number: '12', img: './Images/MIKES.png', text: '"When Triplet Mike kept crashing our Zoom meeting"', name:'- Kremmy' },
     { number: '13', img: './Images/CS.png', text: 'text', name:'name' },
     { number: '14', img: './Images/CS.png', text: 'text', name:'name' },
     { number: '15', img: './Images/CS.png', text: 'text', name:'name' },
+    { number: '4', img: './Images/CAL.png', text: '"The debut of the Caltato"', name: '- Kremmy' },
     { number: '16', img: './Images/CS.png', text: 'text', name:'name' },
-    { number: '17', img: './Images/CS.png', text: 'text', name:'name' },
     { number: '18', img: './Images/CS.png', text: 'text', name:'name' },
     { number: '19', img: './Images/SOM.png', text: '"Who wore it best?"', name:'- you decide' },
     { number: '20', img: './Images/BT.png', text: '"Again...Who wore it best though?"', name:'- you must decide' },
@@ -61,6 +61,7 @@ window.addEventListener('DOMContentLoaded', () => {
       number: Number(container.children[1].textContent),
       element: container,
       isContentShown: false,
+      isPolaroid: false,
     }
     return doorContainersInfo.push(data)
   })
@@ -70,29 +71,35 @@ window.addEventListener('DOMContentLoaded', () => {
     obj.element.addEventListener('click', () => handleDoorChange(obj, true))
   })
 
-  function handleDoorChange(obj, polaroid = false) {
+  function handleDoorChange(obj, onClick = false) {
     const container = obj.element
     const doorBackground = container.children[1]
     const doorPresentContent = container.children[0]
     const doorImageContainer = doorPresentContent.children[0]
+    const doorTextContent = doorImageContainer.children[1]
     const doorName = doorPresentContent.children[1]
 
     if (!obj.isContentShown) {
-      if (polaroid) {
+      if (onClick) {
         unmakeAsPolaroidOnAll()
         makeAsPolaroid(container, doorPresentContent, doorImageContainer, doorName)
+        showText(doorTextContent, doorName)
+        obj.isPolaroid = true
       }
       showContent(doorBackground, doorPresentContent)
     }
 
     else {
-      if (polaroid) {
+      if (onClick) {
         unmakeAsPolaroid(container, doorPresentContent, doorImageContainer, doorName)
+        removeText(doorTextContent, doorName)
       }
       unshowContent(doorBackground, doorPresentContent)
-    }
 
-    obj.isContentShown = !obj.isContentShown
+    }
+    if (obj.isPolaroid) {
+      obj.isContentShown = !obj.isContentShown
+    } 
   }
 
   function showContent(doorBackground, doorPresentContent) {
@@ -124,12 +131,26 @@ window.addEventListener('DOMContentLoaded', () => {
     doorName.classList.remove('polaroid-name')
   }
 
+  function removeText (doorTextContent, doorName) {
+    doorTextContent.classList.add('hidden')
+    doorName.classList.add('hidden')
+}
+
+  function showText (doorTextContent, doorName) {  
+    doorTextContent.classList.remove('hidden')
+    doorName.classList.remove('hidden')
+  }
+
+
   function unmakeAsPolaroidOnAll() {
     doorContainersInfo.forEach(obj => {
       const container = obj.element
       const doorPresentContent = container.children[0]
       const doorImageContainer = doorPresentContent.children[0]
       const doorName = doorPresentContent.children[1]
+      const doorTextContent = doorImageContainer.children[1]
+      obj.isPolaroid = false
+      removeText(doorTextContent, doorName)
       unmakeAsPolaroid(container, doorPresentContent, doorImageContainer, doorName)
     })
   }
@@ -137,7 +158,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // Show boxes of dates already passed
   function openPastDoors() {
     const date = new Date()
-    const previousDay = date.getDate() - 1
+    // const previousDay = date.getDate() - 1
+    const previousDay = 5
 
     if (previousDay) {
       const previousDays = Array.from(new Array(previousDay), (x, i) => i + 1)
@@ -211,9 +233,11 @@ window.addEventListener('DOMContentLoaded', () => {
     // Create p tag and text content
     const pTag = document.createElement('p')
     pTag.textContent = doorInfo.text
+    pTag.classList.add('hidden')
 
     const nameTag= document.createElement('h2')
     nameTag.textContent = doorInfo.name
+    nameTag.classList.add('hidden')
 
     // Append the image and p tag to the present element
     imgContainerElement.appendChild(imgTag)
